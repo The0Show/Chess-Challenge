eacb5ddc71ccda78bc2ff934a92daf8caaab0d58
The0Show <62229104+The0Show@users.noreply.github.com>
2023-07-30 22:09:11 -0400

    uhhhhhh

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,35 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
+        Random rand = new();
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move move = moves[rand.Next(moves.Length)];
+
+        for (int i = 0; i < moves.Length; i++)
+        {
+            if (moves[i].IsCapture || MoveTest(board, moves[i], timer)) move = moves[i];
+        }
+
+        return move;
+    }
+
+    bool MoveTest(Board board, Move move, Timer timer)
+    {
+        if (timer.MillisecondsRemaining <= 10000) return true; // lmao
+
+        board.MakeMove(move);
+
+        bool isABadMove;
+        isABadMove = board.IsInCheckmate();
+        //if (!isABadMove) isABadMove = board.IsInCheck();
+        if (!isABadMove) isABadMove = board.IsInsufficientMaterial();
+
+        board.UndoMove(move);
+
+        return isABadMove;
     }
 }
